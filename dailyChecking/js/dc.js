fb85fc98de79856f89d31f95097ea596061ca7e6
Ottigan <64630527+Ottigan@users.noreply.github.com>
2020-06-12 21:16:49 +0300

    Fixed counter class ('.valid') assignment for recently added rows in case of using SORT

--- dailyChecking/js/dc.js
+++ dailyChecking/js/dc.js
@@ -22,12 +22,13 @@ const loginForm = document.getElementById("login-form"),
 	rowManip = document.querySelector("#row-manipulator"),
 	gameTableNames = document.getElementById("names"),
 	casinoNames = document.getElementById("casinos"),
-	allCounters = document.querySelectorAll('.counter'),
 	auth = firebase.auth(),
 	db = firebase.firestore();
 
 let userUID;
 let tableRows;
+let allCounters = document.querySelectorAll('.counter');
+let allTargets;
 let tablesDB;
 let casinosDB;
 let inputElements = document.querySelectorAll(".inputElement");
@@ -80,7 +81,7 @@ firebase.auth().onAuthStateChanged(dailyCheckingUser => {
 			default:
 				qa = '';
 		}
-		greeting.innerText = `Welcome, ${qa}!`;
+		greeting.innerText = `Welcome, ${ qa }!`;
 		greeting.style.cssText = 'margin-bottom: -2px; align-self: flex-end; color: white; visibility: visible; font-family: Georgia, "Times New Roman", Times, serif; font-weight: 400';
 		logoutButton.before(greeting);
 
@@ -117,19 +118,19 @@ firebase.auth().onAuthStateChanged(dailyCheckingUser => {
 						let i = 0;
 						do {
 							if (i === 0) {
-								document.querySelector(`#table-${i}`).value = rowObjects[i].name;
-								document.querySelector(`#platform-${i}`).value =
+								document.querySelector(`#table-${ i }`).value = rowObjects[i].name;
+								document.querySelector(`#platform-${ i }`).value =
 									rowObjects[i].platform;
-								document.querySelector(`#casino-${i}`).value = rowObjects[i].casino;
-								document.querySelector(`#counter-${i}`).innerHTML = rowObjects[i].counter || 0;
-								document.querySelector(`#target-${i}`).value = rowObjects[i].target;
+								document.querySelector(`#casino-${ i }`).value = rowObjects[i].casino;
+								document.querySelector(`#counter-${ i }`).innerHTML = rowObjects[i].counter || 0;
+								document.querySelector(`#target-${ i }`).value = rowObjects[i].target;
 							} else if (i > 0) {
 								const rowItem = document.createElement("form");
 								rowItem.classList.add("flex", "jc-c", "table-row");
 								rowItem.innerHTML = `<div>
 									<input type="text" name="table" pattern="[a-zA-Z0-9 ]+" list="names" class="inputElement highlight-this" autocomplete="off" id="table-${
 									rowObjects[i].id
-									}" value="${rowObjects[i].name}"/>
+									}" value="${ rowObjects[i].name }"/>
 									</div>
 									<div>
 									<input id="platform-${
@@ -141,11 +142,11 @@ firebase.auth().onAuthStateChanged(dailyCheckingUser => {
 									<div>
 									<input type="text" name="casino" list="casinos" class="inputElement highlight-this" autocomplete="off" id="casino-${
 									rowObjects[i].id
-									}" value="${rowObjects[i].casino.toLowerCase()}"/>
+									}" value="${ rowObjects[i].casino.toLowerCase() }"/>
 									</div>
 									<span id="counter-${
 									rowObjects[i].id
-									}" class="counter highlight-this">${rowObjects[i].counter || 0}</span>
+									}" class="counter highlight-this">${ rowObjects[i].counter || 0 }</span>
 									<input id="target-${
 									rowObjects[i].id
 									}" type="number" class="target highlight-this" value="${
@@ -208,6 +209,7 @@ logoutButton.addEventListener('click', function () {
 		});
 });
 
+//Row addition and removal
 //Chaining promise requests from firestore to sync DB info with client info
 //During the promise chaining buttons are disabled, to avoid information desync
 const manipRows = (event) => {
@@ -233,6 +235,7 @@ const manipRows = (event) => {
 						})
 						.then(function () {
 							row.remove();
+							tableRows = document.querySelectorAll(".table-row");
 							target.removeAttribute('disabled');
 						})
 						.catch(function (error) {
@@ -261,17 +264,17 @@ const manipRows = (event) => {
 						const rowItem = document.createElement("form");
 						rowItem.classList.add("flex", "jc-c", "table-row");
 						rowItem.innerHTML = `<div>
-							<input type="text" name="table" list="names" class="inputElement" autocomplete="off" pattern="[a-zA-Z0-9]+" id="table-${id}" />
+							<input type="text" name="table" list="names" class="inputElement" autocomplete="off" pattern="[a-zA-Z0-9]+" id="table-${id }" />
 							</div>
 							<div>
-							<input id="platform-${id}" name="platform" type="text" list="platforms" autocomplete="off"/>
+							<input id="platform-${id }" name="platform" type="text" list="platforms" autocomplete="off"/>
 							</div>
 							<div>
-							<input type="text" name="casino" id="casino-${id}" list="casinos" class="inputElement" autocomplete="off"/>
+							<input type="text" name="casino" id="casino-${id }" list="casinos" class="inputElement" autocomplete="off"/>
 							</div>
-							<span id="counter-${id}" class="counter highlight-this invalid">0</span>
-							<input id="target-${id}" type="number" class="target highlight-this" value="1" maxlength="2" min="0" max="12" />
-							<button id="${id}" class="submitButton" type="button">
+							<span id="counter-${id }" class="counter highlight-this invalid">0</span>
+							<input id="target-${id }" type="number" class="target highlight-this" value="1" maxlength="2" min="0" max="12" />
+							<button id="${id }" class="submitButton" type="button">
 							Submit
 							</button>`;
 						rowManip.before(rowItem);
@@ -289,6 +292,7 @@ const manipRows = (event) => {
 							})
 							.then(function () {
 								console.log('Row successfully added!');
+								tableRows = document.querySelectorAll(".table-row");
 								target.removeAttribute('disabled');
 							})
 							.catch(function (error) {
@@ -358,12 +362,12 @@ const updateCounterAndOptions = (event) => {
 
 	if (target.classList.contains("highlight-this")) {
 		let indexID = target.id.substring(target.id.indexOf("-") + 1),
-			tableName = document.querySelector(`#table-${indexID}`),
-			platform = document.querySelector(`#platform-${indexID}`),
-			casino = document.querySelector(`#casino-${indexID}`),
-			counter = document.querySelector(`#counter-${indexID}`),
-			targetNumber = document.querySelector(`#target-${indexID}`),
-			submitButton = document.getElementById(`${indexID}`);
+			tableName = document.querySelector(`#table-${ indexID }`),
+			platform = document.querySelector(`#platform-${ indexID }`),
+			casino = document.querySelector(`#casino-${ indexID }`),
+			counter = document.querySelector(`#counter-${ indexID }`),
+			targetNumber = document.querySelector(`#target-${ indexID }`),
+			submitButton = document.getElementById(`${ indexID }`);
 
 		if (event.type === "mouseover") {
 			tableName.classList.add('highlighted-row');
@@ -384,11 +388,11 @@ const updateCounterAndOptions = (event) => {
 
 	if (target.classList.contains("submitButton") && event.type === "click") {
 
-		let tableName = document.querySelector(`#table-${target.id}`).value,
-			platform = document.querySelector(`#platform-${target.id}`).value,
-			casino = document.querySelector(`#casino-${target.id}`).value,
-			counter = document.querySelector(`#counter-${target.id}`),
-			goal = document.querySelector(`#target-${target.id}`);
+		let tableName = document.querySelector(`#table-${ target.id }`).value,
+			platform = document.querySelector(`#platform-${ target.id }`).value,
+			casino = document.querySelector(`#casino-${ target.id }`).value,
+			counter = document.querySelector(`#counter-${ target.id }`),
+			goal = document.querySelector(`#target-${ target.id }`);
 
 		let x = counter.innerHTML,
 			y = goal.value;
@@ -478,6 +482,24 @@ const updateCounterAndOptions = (event) => {
 			tableRows[i][3].value = newTableRowOrder[i][3].value;
 		}
 
+		allCounters = document.querySelectorAll('.counter');
+		allTargets = document.querySelectorAll('.target')
+
+		for (let i = 0; i < allCounters.length; i++) {
+			let counter = allCounters[i];
+			let x = Number.parseInt(allCounters[i].innerHTML);
+			let y = allTargets[i].value;
+
+			if (x >= y) {
+				counter.classList.add("valid");
+				counter.classList.remove("invalid");
+			} else {
+				counter.classList.add("invalid");
+				counter.classList.remove("valid");
+			}
+		}
+
+
 		// getting the entire firestore array, because you can't update specific values in the cloud
 		db.collection("dailyChecking")
 			//changing the following userUID helps copying row state between users
@@ -486,12 +508,16 @@ const updateCounterAndOptions = (event) => {
 			.then(function (doc) {
 				let rowObjects = doc.data().rowObjects;
 				rowObjects.forEach(object => {
-					object.name = document.getElementById(`table-${object.id}`).value;
-					object.platform = document.getElementById(`platform-${object.id}`).value;
-					object.casino = document.getElementById(`casino-${object.id}`).value;
-					object.counter = Number.parseInt(document.getElementById(`counter-${object.id}`).innerHTML);
-					object.target = Number.parseInt(document.getElementById(`target-${object.id}`).value);
+					object.name = document.getElementById(`table-${ object.id }`).value;
+					object.platform = document.getElementById(`platform-${ object.id }`).value;
+					object.casino = document.getElementById(`casino-${ object.id }`).value;
+					object.counter = Number.parseInt(document.getElementById(`counter-${ object.id }`).innerHTML);
+					object.target = Number.parseInt(document.getElementById(`target-${ object.id }`).value);
 				});
+
+				for (let i = 0; i < tableRows.length; i++) {
+
+				}
 				db.collection("dailyChecking")
 					.doc(userUID)
 					.update({
@@ -540,7 +566,7 @@ const updateCounterAndOptions = (event) => {
 				console.log(error);
 			});
 	} else if (target.classList.contains('target') && event.type === "change") {
-		let counter = document.getElementById(`counter-${target.id.substring(target.id.indexOf("-") + 1)}`),
+		let counter = document.getElementById(`counter-${ target.id.substring(target.id.indexOf("-") + 1) }`),
 			goal = target;
 
 		let x = Number.parseInt(counter.innerHTML),
@@ -560,6 +586,5 @@ const updateCounterAndOptions = (event) => {
 checkRows.addEventListener("click", updateCounterAndOptions);
 checkRows.addEventListener("keyup", updateCounterAndOptions);
 checkRows.addEventListener("mouseover", updateCounterAndOptions);
-checkRows.addEventListener("mouseout", updateCounterAndOptions);
 checkRows.addEventListener("mouseout", updateCounterAndOptions);
 checkRows.addEventListener("change", updateCounterAndOptions);
